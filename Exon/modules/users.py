35561c47a0c5968df32606c46fcca6a8129c6fe3
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-10-05 23:11:43 +0530

    Update users.py

--- Exon/modules/users.py
+++ Exon/modules/users.py
@@ -205,11 +205,11 @@ def chat_checker(update: Update, context: CallbackContext):
 
 def __user_info__(user_id):
     if user_id in [777000, 1087968824]:
-        return """<b>ɢʀᴏᴜᴘs ᴄᴏᴜɴᴛ</b>: idk :p</code>"""
+        return """╘══「 Groups count: <code>???</code> 」"""
     if user_id == dispatcher.bot.id:
-        return """<b>ɢʀᴏᴜᴘs ᴄᴏᴜɴᴛ</b>: idk :p</code>"""
+        return """╘══「 Groups count: <code>???</code> 」"""
     num_chats = sql.get_user_num_chats(user_id)
-    return f"""ɢʀᴏᴜᴘs ᴄᴏᴜɴᴛ: <code>{num_chats}</code>"""
+    return f"""╘══「 Groups count: <code>{num_chats}</code> 」"""
 
 
 def __stats__():
