fa50edad19d7b75a7bae1ededc3c474ff57c9201
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-10-09 00:05:24 +0530

    Update purge.py

--- Exon/modules/purge.py
+++ Exon/modules/purge.py
@@ -1,32 +1,33 @@
 """
-MIT License
+STATUS: Code is working. ✅
+"""
 
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
+"""
+GNU General Public License v3.0
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+Credits:-
+    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import time
-
 from telethon import events
 
-from Exon import telethn
+from Exon import telethn, DEV_USERS
 from Exon.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
@@ -38,25 +39,29 @@ async def purge_messages(event):
     if event.from_id is None:
         return
 
-    if not await user_is_admin(
-        user_id=event.sender_id,
-        message=event,
-    ) and event.from_id not in [1087968824]:
-        await event.reply("ᴏɴʟʏ ᴀᴅᴍɪɴs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
+    if (
+        not await user_is_admin(
+            user_id=event.sender_id,
+            message=event,
+        )
+        and event.from_id not in DEV_USERS
+    ):
+        await event.reply("Only Admins are allowed to use this command")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("ᴄᴀɴ'ᴛ sᴇᴇᴍ ᴛᴏ ᴘᴜʀɢᴇ ᴛʜᴇ ᴍᴇssᴀɢᴇ")
+        await event.reply("Can't seem to purge the message")
         return
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴇʟᴇᴄᴛ ᴡʜᴇʀᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢɪɴɢ ғʀᴏᴍ.")
+        await event.reply("Reply to a message to select where to start purging from.")
         return
+    messages = []
     message_id = reply_msg.id
     delete_to = event.message.id
 
-    messages = [event.reply_to_msg_id]
+    messages.append(event.reply_to_msg_id)
     for msg_id in range(message_id, delete_to + 1):
         messages.append(msg_id)
         if len(messages) == 100:
@@ -67,34 +72,35 @@ async def purge_messages(event):
         await event.client.delete_messages(event.chat_id, messages)
     except:
         pass
-    time.perf_counter() - start
-    text = "shhh!"
+    time_ = time.perf_counter() - start
+    text = f"Purged Successfully in {time_:0.2f} Second(s)"
     await event.respond(text, parse_mode="markdown")
-
 
 async def delete_messages(event):
     if event.from_id is None:
         return
 
-    if not await user_is_admin(
-        user_id=event.sender_id,
-        message=event,
-    ) and event.from_id not in [1087968824]:
-        await event.reply("ᴏɴʟʏ ᴀᴅᴍɪɴs ᴀʀᴇ ᴀʟʟᴏᴡᴇᴅ ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
+    if (
+        not await user_is_admin(
+            user_id=event.sender_id,
+            message=event,
+        )
+        and event.from_id not in DEV_USERS
+    ):
+        await event.reply("Only Admins are allowed to use this command")
         return
 
     if not await can_delete_messages(message=event):
-        await event.reply("ᴄᴀɴ'ᴛ sᴇᴇᴍ ᴛᴏ ᴅᴇʟᴇᴛᴇ ᴛʜɪs?")
+        await event.reply("Can't seem to delete this?")
         return
 
     message = await event.get_reply_message()
     if not message:
-        await event.reply("ᴡʜᴀᴛ, ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ?")
+        await event.reply("Whadya want to delete?")
         return
     chat = await event.get_input_chat()
     del_message = [message, event.message]
     await event.client.delete_messages(chat, del_message)
-
 
 PURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]purge$")
 DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
