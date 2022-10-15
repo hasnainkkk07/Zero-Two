f3e7c5dd85ba181b59fb2660df85ca58450385e0
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-10-16 00:16:25 +0530

    Update shippering.py

--- Exon/modules/shippering.py
+++ Exon/modules/shippering.py
@@ -1,35 +1,35 @@
 """
-MIT License
-
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+STATUS: Code is working. ✅
 """
 
+"""
+GNU General Public License v3.0
+
+Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING]
+
+Credits:-
+    I don't know who originally wrote this code. If you originally wrote this code, please reach out to me. 
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from Exon import pbot as app
+from Exon.utils.errors import capture_err
+from Exon.Extras.dbfunctions import get_couple, save_couple
+from pyrogram import filters
 import random
 from datetime import datetime
-
-from pyrogram import filters
-
-from Exon import pgram
-from Exon.modules.mongo.couples_mongo import get_couple, save_couple
-from Exon.utils.errors import capture_err
 
 
 # Date and time
@@ -55,49 +55,53 @@ today = str(dt()[0])
 tomorrow = str(dt_tom())
 
 
-@pgram.on_message(filters.command("couples") & ~filters.edited)
-@capture_err
+
+@app.on_message(filters.command("couples"))
 async def couple(_, message):
     if message.chat.type == "private":
-        await message.reply_text("ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋs ɪɴ ɢʀᴏᴜᴘs.")
+        await message.reply_text("This command only works in groups.")
         return
     try:
         chat_id = message.chat.id
         is_selected = await get_couple(chat_id, today)
         if not is_selected:
             list_of_users = []
-            async for i in pgram.iter_chat_members(message.chat.id):
+            async for i in app.get_chat_members(message.chat.id):
                 if not i.user.is_bot:
                     list_of_users.append(i.user.id)
             if len(list_of_users) < 2:
-                await message.reply_text("ɴᴏᴛ ᴇɴᴏᴜɢʜ ᴜsᴇʀs")
+                await message.reply_text("Not enough users")
                 return
             c1_id = random.choice(list_of_users)
             c2_id = random.choice(list_of_users)
             while c1_id == c2_id:
                 c1_id = random.choice(list_of_users)
-            c1_mention = (await pgram.get_users(c1_id)).mention
-            c2_mention = (await pgram.get_users(c2_id)).mention
+            c1_mention = (await app.get_users(c1_id)).mention
+            c2_mention = (await app.get_users(c2_id)).mention
 
-            couple_selection_message = f"""**ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ:**
-{c1_mention} + {c2_mention} = 💗
-__ɴᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴍᴀʏ ʙᴇ ᴄʜᴏsᴇɴ ᴀᴛ 12ᴀᴍ {ᴛᴏᴍᴏʀʀᴏᴡ}__"""
-            await pgram.send_message(message.chat.id, text=couple_selection_message)
+            couple_selection_message = f"""**Couple of the day:**
+{c1_mention} + {c2_mention} = ❤️
+__New couple of the day may be chosen at 12AM {tomorrow}__"""
+            await app.send_message(message.chat.id, text=couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
             await save_couple(chat_id, today, couple)
 
         elif is_selected:
             c1_id = int(is_selected["c1_id"])
             c2_id = int(is_selected["c2_id"])
-            c1_name = (await pgram.get_users(c1_id)).first_name
-            c2_name = (await pgram.get_users(c2_id)).first_name
-            couple_selection_message = f"""ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ:
-[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = 💜
-__ɴᴇᴡ ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ ᴍᴀʏ ʙᴇ ᴄʜᴏsᴇɴ ᴀᴛ 12ᴀᴍ {ᴛᴏᴍᴏʀʀᴏᴡ}__"""
-            await pgram.send_message(message.chat.id, text=couple_selection_message)
+            c1_name = (await app.get_users(c1_id)).first_name
+            c2_name = (await app.get_users(c2_id)).first_name
+            couple_selection_message = f"""Couple of the day:
+[{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
+__New couple of the day may be chosen at 12AM {tomorrow}__"""
+            await app.send_message(message.chat.id, text=couple_selection_message)
     except Exception as e:
         print(e)
         await message.reply_text(e)
 
 
-__mod_name__ = "𝙲ᴏᴜᴘʟᴇs"
+__mod_name__ = "Couples 💑"
+__help__ = """
+*Couples / Shippering*
+ ❍ `/couples` or `/Shippering` :  get couples of the day!
+"""
